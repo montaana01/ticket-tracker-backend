a55b00023e8e2ab488a18dccdb7f4c68f3a10e2b
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-20 00:24:19 +0300

    chore: set up base config

---
+++ src/config/params-example.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'hostname' => 'hostname',
+        'port' => '3306',
+        'dbname' => 'dbname',
+        'username' => 'username',
+        'password' => 'password',
+    ],
+];
