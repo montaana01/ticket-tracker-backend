4e3e341cccf987cdbaa2ea04a2be8c075eeaba8b
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-21 22:40:59 +0300

    feat: implement jwt auth controller

---
+++ src/helpers/JwtAuth.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TicketTracker\Helpers;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JwtAuth
+{
+    private string $token;
+
+    public function __construct()
+    {
+        $key = require __DIR__ . '/../config/params.php';
+        $this->token = $key['JWT'];
+    }
+
+    public function generateToken(int $userId, string $role, int $ttlHours = 24): string {
+        $issuedAt = time();
+        $payload = [
+            'iss' => 'TicketTracker',
+            'sub' => $userId,
+            'role' => $role,
+            'iat' => $issuedAt,
+            'exp' => $issuedAt + ($ttlHours * 3600)
+        ];
+        return JWT::encode($payload, $this->token , 'HS256');
+    }
+
+    public function validateToken(string $token): \stdClass
+    {
+        return JWT::decode($token, new Key($this->token, 'HS256'));
+    }
+}
