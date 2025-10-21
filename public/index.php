f343f862e3d37a56633fc8f978ca5050d069e417
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-21 22:45:09 +0300

    feat: update index file

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use TicketTracker\Controllers\UserController;
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 header('Content-Type: application/json');
 // Basic workpiece with getting basic users info from db
 try {
@@ -13,3 +17,4 @@ try {
     http_response_code(500);
     echo "An error occurred: " . $error->getMessage();
 }
+var_dump($_SERVER);exit();
