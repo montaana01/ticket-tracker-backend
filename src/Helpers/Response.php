883cfedd8ab70c78d737857d4f08f53fca327d01
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-10-25 00:06:23 +0300

    refactor: update response class return type

--- src/Helpers/Response.php
+++ src/Helpers/Response.php
@@ -2,7 +2,7 @@
 namespace TicketTracker\Helpers;
 
 class Response {
-    public static function json($data, int $status = 200): void {
+    public static function json($data, int $status = 200): array {
         http_response_code($status);
         header('Content-Type: application/json');
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
