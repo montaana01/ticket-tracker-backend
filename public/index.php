66efd0e4bc8f22c7142a1b5d0e77ee06a2ceaa1b
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-27 23:23:23 +0300

    feat: enable CORS from localhost

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 header("Access-Control-Allow-Origin: https://tickets.yakovlevdev.com");
+header("Access-Control-Allow-Origin: localhost");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
